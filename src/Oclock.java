4f9de2848e275d38ba0847bc69649e054c9b5d59
khoand10@gmail.com <khoand10@gmail.com>
2018-04-20 08:31:12 +0700

    bài tập đồng hồ

---
+++ src/Oclock.java
@@ -0,0 +1,21 @@
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Oclock {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(LocalDateTime.now());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+}
